3b4037cd31417af9cee1db1751c3404ebbb8682b
RT <beat1422@icloud.com>
2019-10-24 14:08:45 -0700

    added git ignore

---
+++ js-6/fetch_api_assignment.js
@@ -0,0 +1,4 @@
+fetch('https://cors-anywhere.herokuapp.com/https://github.com/facebook/react/issues')
+    .then(res => res.json()) 
+    .then((result)=>{console.log(result)})
+    .catch(err => console.log(err))
